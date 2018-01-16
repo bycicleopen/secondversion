e071377b8c1f45fcbe6acd149ef693c21096a1fd
Z-Warm <zwarm@yandex.ru>
2018-01-16 12:19:06 +0200

    firstmaster

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/basic/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/basic/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/usr/www/basic/catalog/');
+define('DIR_SYSTEM', 'D:/usr/www/basic/system/');
+define('DIR_IMAGE', 'D:/usr/www/basic/image/');
+define('DIR_LANGUAGE', 'D:/usr/www/basic/catalog/language/');
+define('DIR_TEMPLATE', 'D:/usr/www/basic/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/usr/www/basic/system/config/');
+define('DIR_CACHE', 'D:/usr/www/basic/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/usr/www/basic/system/storage/download/');
+define('DIR_LOGS', 'D:/usr/www/basic/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/usr/www/basic/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/usr/www/basic/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'basic');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'bcc_');
