5f205b5c3a37de72c484f2e31f473c792be6fab9
Z-Warm <zwarm@yandex.ru>
2018-02-03 14:40:02 +0200

    secondbasic

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://localhost/basic/admin/');
 define('HTTPS_CATALOG', 'http://localhost/basic/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/usr/www/basic/admin/');
-define('DIR_SYSTEM', 'D:/usr/www/basic/system/');
-define('DIR_IMAGE', 'D:/usr/www/basic/image/');
-define('DIR_LANGUAGE', 'D:/usr/www/basic/admin/language/');
-define('DIR_TEMPLATE', 'D:/usr/www/basic/admin/view/template/');
-define('DIR_CONFIG', 'D:/usr/www/basic/system/config/');
-define('DIR_CACHE', 'D:/usr/www/basic/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/usr/www/basic/system/storage/download/');
-define('DIR_LOGS', 'D:/usr/www/basic/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/usr/www/basic/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/usr/www/basic/system/storage/upload/');
-define('DIR_CATALOG', 'D:/usr/www/basic/catalog/');
+define('DIR_APPLICATION', 'h:/usr/www/basic_second/admin/');
+define('DIR_SYSTEM', 'h:/usr/www/basic_second/system/');
+define('DIR_IMAGE', 'h:/usr/www/basic_second/image/');
+define('DIR_LANGUAGE', 'h:/usr/www/basic_second/admin/language/');
+define('DIR_TEMPLATE', 'h:/usr/www/basic_second/admin/view/template/');
+define('DIR_CONFIG', 'h:/usr/www/basic_second/system/config/');
+define('DIR_CACHE', 'h:/usr/www/basic_second/system/storage/cache/');
+define('DIR_DOWNLOAD', 'h:/usr/www/basic_second/system/storage/download/');
+define('DIR_LOGS', 'h:/usr/www/basic_second/system/storage/logs/');
+define('DIR_MODIFICATION', 'h:/usr/www/basic_second/system/storage/modification/');
+define('DIR_UPLOAD', 'h:/usr/www/basic_second/system/storage/upload/');
+define('DIR_CATALOG', 'h:/usr/www/basic_second/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://localhost/basic/');
 define('HTTPS_SERVER', 'http://localhost/basic/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/usr/www/basic/catalog/');
-define('DIR_SYSTEM', 'D:/usr/www/basic/system/');
-define('DIR_IMAGE', 'D:/usr/www/basic/image/');
-define('DIR_LANGUAGE', 'D:/usr/www/basic/catalog/language/');
-define('DIR_TEMPLATE', 'D:/usr/www/basic/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/usr/www/basic/system/config/');
-define('DIR_CACHE', 'D:/usr/www/basic/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/usr/www/basic/system/storage/download/');
-define('DIR_LOGS', 'D:/usr/www/basic/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/usr/www/basic/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/usr/www/basic/system/storage/upload/');
+define('DIR_APPLICATION', 'h:/usr/www/basic_second/catalog/');
+define('DIR_SYSTEM', 'h:/usr/www/basic_second/system/');
+define('DIR_IMAGE', 'h:/usr/www/basic_second/image/');
+define('DIR_LANGUAGE', 'h:/usr/www/basic_second/catalog/language/');
+define('DIR_TEMPLATE', 'h:/usr/www/basic_second/catalog/view/theme/');
+define('DIR_CONFIG', 'h:/usr/www/basic_second/system/config/');
+define('DIR_CACHE', 'h:/usr/www/basic_second/system/storage/cache/');
+define('DIR_DOWNLOAD', 'h:/usr/www/basic_second/system/storage/download/');
+define('DIR_LOGS', 'h:/usr/www/basic_second/system/storage/logs/');
+define('DIR_MODIFICATION', 'h:/usr/www/basic_second/system/storage/modification/');
+define('DIR_UPLOAD', 'h:/usr/www/basic_second/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
